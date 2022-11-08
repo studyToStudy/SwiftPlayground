964659eaa0a464e1931b3f773b42aa3882a58c13
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-08 18:36:14 +0900

    feat: 함수의 종류

--- Swift Playground.playground/Pages/Func.xcplaygroundpage/Contents.swift
+++ Swift Playground.playground/Pages/Func.xcplaygroundpage/Contents.swift
@@ -2,5 +2,21 @@
 
 import Swift
 
-var greeting = "Hello, playground"
+//Mark: 매개변수가 없는 함수
+//func sayHello(){
+//    print("안녕하세요?")
+//}
+//sayHello()
 
+func sayHello(name: String = "익명") {
+    print("안녕하세요 \(name)입니다.")
+}
+
+sayHello() // 기본값이 설정된 값
+sayHello(name: "정후") // 매개변수가 들어간 값
+
+func greet(_ name: String = "익명") {
+    print("안녕하세요 \(name)입니다.")
+}
+
+greet("이정후") // 언더바(_)를 사용하면 name을 넣지 않아도 된다.
