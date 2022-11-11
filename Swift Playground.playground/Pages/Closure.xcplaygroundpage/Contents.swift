6f4e3ebe895b9de65622ddf82e1dd99eecc9f79f
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-11 09:37:06 +0900

    Closure Study

---
+++ Swift Playground.playground/Pages/Closure.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+// 매개변수 X, 반환 X
+//func someFunction() -> Void {
+//    print(#function)
+//}
+// 함수 호출
+//someFunction()
+
+// Mark:
+// 클로져, 매개변수 X, 반환 X
+// 클로져 선언
+// 자료형이 () -> Void
+// () -> () 이것도 가능
+// 매개변수도 없고 반환값도 없는것이 자료형이다.
+//let someClosure: () -> Void = {
+//    print("someClosuer입니다.")
+//    return
+//}
+//someClosure()
+
+// 매개변수 O, 반환 X
+func someFunction(name: String) -> Void {
+    print(#function + "name: \(name)")
+    return
+}
+// 매개변수를 가진 클로저는 튜플의 형태로 받을 수 있다. + in
+let someClosure: (_ name: String) -> Void = { (name: String) in
+    print(#function + "name: \(name)")
+    return
+}
+someFunction(name: "이정후")
+someClosure("이정후입니다")
